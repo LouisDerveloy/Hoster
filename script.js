eb9107520479e0c317eec19a7817d0ce10d319f5
Louis Derveloy <louis.derveloy@gmail.com>
2022-11-26 01:33:21 +0100

    Add swiper for client opinions

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 var princingPlans = document.getElementById("pricingPlans");
 var navbar = document.getElementById("navbar-navbar");
+
+const clientCommentSwiper = new Swiper('.clientOpinion-content-opinion', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.clientOpinion-content-arrow-right',
+      prevEl: '.clientOpinion-content-arrow-left',
+    },
+    slidesPerView: 1,
+    spaceBetween: 50,
+    autoplay: {
+        delay: 5000,
+    },
+    breakpoints: {
+        700: {
+          slidesPerView: 2
+        },
+        1023: {
+          slidesPerView: 3,
+        },
+    },
+  });
