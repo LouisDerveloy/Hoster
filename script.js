b571fb29ae5b53c69dee0d3ca2d47febe366d13d
Louis Derveloy <louis.derveloy@gmail.com>
2022-12-11 02:17:51 +0100

    Add appearance animation not responsive

--- script.js
+++ script.js
@@ -2,26 +2,51 @@ var princingPlans = document.getElementById("pricingPlans");
 var navbar = document.getElementById("navbar-navbar");
 
 const clientCommentSwiper = new Swiper('.clientOpinion-content-opinion', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.clientOpinion-content-arrow-right',
-      prevEl: '.clientOpinion-content-arrow-left',
-    },
-    slidesPerView: 1,
-    spaceBetween: 50,
-    autoplay: {
-        delay: 5000,
-    },
-    breakpoints: {
-        700: {
-          slidesPerView: 2
-        },
-        1023: {
-          slidesPerView: 3,
-        },
-    },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.clientOpinion-content-arrow-right',
+    prevEl: '.clientOpinion-content-arrow-left',
+  },
+  slidesPerView: 1,
+  spaceBetween: 50,
+  autoplay: {
+      delay: 5000,
+  },
+  breakpoints: {
+      700: {
+        slidesPerView: 2
+      },
+      1023: {
+        slidesPerView: 3,
+      },
+  },
+});
+
+var crossInElements = document.getElementsByClassName("CrossIn");
+var options = {
+  rootMargin: "-10%",
+  threshold: 0.1,
+};
+
+function intersectionObserverCallback(objects, observer) {
+  objects.forEach(element => {
+    if (element.isIntersecting) {
+      observer.unobserve(element.target);
+      element.target.classList.add("CrossIn-active");
+    }
+
+    console.log(element);
   });
+  return observer;
+}
+
+var intersectionObserver = new IntersectionObserver(intersectionObserverCallback, options);
+
+for (let index = 0; index < crossInElements.length; index++) {
+  const element = crossInElements[index];
+  intersectionObserver.observe(element);
+}
